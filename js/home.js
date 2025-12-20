54eba46387a4c412caa075325194a347f7ce4ca5
Jose Bolivar <tu-correo@ejemplo.com>
2025-12-20 10:44:55 -0300

    commit

--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
 // home.js
+import { API_BASE } from "./config.js";
 import { initBanners } from "./banners.js";
 import { loadWebContent } from "./webContent.js";
 
@@ -6,3 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
     initBanners();
     loadWebContent();
 });
+
+const directBtn = document.getElementById("directDownloadBtn");
+if (directBtn) {
+    directBtn.href = `${API_BASE}/apk/xaimua`;
+}
