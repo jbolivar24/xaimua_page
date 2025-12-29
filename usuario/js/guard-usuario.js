7012f19fa0b9440cb72a88c13bf1b7702556a95a
Jose Bolivar <tu-correo@ejemplo.com>
2025-12-29 12:19:23 -0300

    commit

--- usuario/js/guard-usuario.js
+++ usuario/js/guard-usuario.js
@@ -1,9 +1,11 @@
 // usuario/js/guard-usuario.js
-import { getActiveToken, getActiveRole } from "../../js/auth.js";
-import { buildPath } from "../../js/config.js";
+import { getActiveToken, getActiveRole } from "/xaimua_page/js/auth.js";
+import { buildPath } from "/xaimua_page/js/config.js";
 
 const token = getActiveToken();
 const role  = getActiveRole();
+
+console.log("🛡️ GUARD usuario → token:", token, "role:", role);
 
 if (!token || role !== "USUARIO") {
   window.location.href = buildPath("/html/login.html");
--- vendedor/js/guard-vendedor.js
+++ vendedor/js/guard-vendedor.js
@@ -1,9 +1,11 @@
 // js/guard-vendedor.js
-import { getActiveToken, getActiveRole } from "../../js/auth.js";
-import { buildPath } from "../../js/config.js";
+import { getActiveToken, getActiveRole } from "/xaimua_page/js/auth.js";
+import { buildPath } from "/xaimua_page/js/config.js";
 
 const token = getActiveToken();
 const role  = getActiveRole();
+
+console.log("🛡️ GUARD usuario → token:", token, "role:", role);
 
 if (!token || role !== "VENDEDOR") {
   window.location.href = buildPath("/html/login.html");
