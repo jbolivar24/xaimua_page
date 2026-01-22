cd8553918195a2d5f4ed58ad551ee7562f077ca5
Jose Bolivar <tu-correo@ejemplo.com>
2026-01-22 13:29:41 -0300

    commit

--- js/login.js
+++ js/login.js
@@ -28,9 +28,8 @@ function saveTokenByRole(role, token) {
 
 async function doLogin() {
   const rawUsername = (usernameEl.value || "").trim();
-  const normalized = normalizeRut(rawUsername);
-  const username = normalized ? normalized.replace("-", "") : null;
-
+  //const normalized = rawUsernamenormalizeRut(rawUsername);
+  const username = rawUsername;//normalized ? normalized.replace("-", "") : null;
   const password = (passwordEl.value || "").trim();
 
   if (!username) {
